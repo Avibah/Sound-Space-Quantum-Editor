b685d283a149201474b1a22043e235ae033c909e
Avibah <redstoneminecraft45@gmail.com>
2022-11-25 14:04:18 -0600

    switch changelog location

--- Blox Saber Editor/Gui/GuiScreenMenu.cs
+++ Blox Saber Editor/Gui/GuiScreenMenu.cs
@@ -57,7 +57,7 @@ namespace Sound_Space_Editor.Gui
 			try
             {
 				SecureWebClient wc = new SecureWebClient();
-				ChangelogText = wc.DownloadString("https://raw.githubusercontent.com/David20122/Sound-Space-Quantum-Editor/master/changelog");
+				ChangelogText = wc.DownloadString("https://raw.githubusercontent.com/Avibah/Sound-Space-Quantum-Editor/master/changelog");
 				Changelog = new GuiLabel(0, 0, ChangelogText, "main", 16);
 
 				ScrollBar.MaxValue = ChangelogText.Split('\n').Length;
