1ae17f2b1c1ca7fd4e098eb6973fe88b61896006
Avibah <redstoneminecraft45@gmail.com>
2025-05-06 14:56:07 -0500

    include alpha from given Color in drawn shapes

--- Editor/New SSQE/NewGUI/GLVerts.cs
+++ Editor/New SSQE/NewGUI/GLVerts.cs
@@ -5,94 +5,90 @@ namespace New_SSQE.NewGUI
 {
     internal class GLVerts
     {
-        public static float[] Rect(float x, float y, float w, float h, params float[] c)
+        public static float[] Rect(float x, float y, float w, float h, float r, float g, float b, float a = 1)
         {
-            float a = c.Length == 4 ? c[3] : 1f;
-
             return
             [
-                x, y, c[0], c[1], c[2], a,
-                x + w, y, c[0], c[1], c[2], a,
-                x, y + h, c[0], c[1], c[2], a,
+                x, y, r, g, b, a,
+                x + w, y, r, g, b, a,
+                x, y + h, r, g, b, a,
 
-                x + w, y + h, c[0], c[1], c[2], a,
-                x, y + h, c[0], c[1], c[2], a,
-                x + w, y, c[0], c[1], c[2], a
+                x + w, y + h, r, g, b, a,
+                x, y + h, r, g, b, a,
+                x + w, y, r, g, b, a
             ];
         }
 
-        public static float[] Rect(RectangleF rect, params float[] c)
-            => Rect(rect.X, rect.Y, rect.Width, rect.Height, c);
-        public static float[] Rect(float x, float y, float w, float h, Color color, float alpha = 1)
-            => Rect(x, y, w, h, color.R / 255f, color.G / 255f, color.B / 255f, alpha);
-        public static float[] Rect(RectangleF rect, Color color, float alpha = 1)
-            => Rect(rect.X, rect.Y, rect.Width, rect.Height, color.R / 255f, color.G / 255f, color.B / 255f, alpha);
+        public static float[] Rect(RectangleF rect, float r, float g, float b, float a = 1)
+            => Rect(rect.X, rect.Y, rect.Width, rect.Height, r, g, b, a);
+        public static float[] Rect(float x, float y, float w, float h, Color color, float a = 1)
+            => Rect(x, y, w, h, color.R / 255f, color.G / 255f, color.B / 255f, color.A / 255f * a);
+        public static float[] Rect(RectangleF rect, Color color, float a = 1)
+            => Rect(rect.X, rect.Y, rect.Width, rect.Height, color.R / 255f, color.G / 255f, color.B / 255f, color.A / 255f * a);
 
 
 
-        public static float[] Outline(float x, float y, float w, float h, float lw, params float[] c)
+        public static float[] Outline(float x, float y, float w, float h, float lw, float r, float g, float b, float a = 1)
         {
-            float a = c.Length == 4 ? c[3] : 1f;
-
             x -= lw / 2;
             y -= lw / 2;
 
             return
             [
-                x, y, c[0], c[1], c[2], a,
-                x + w, y, c[0], c[1], c[2], a,
-                x, y + lw, c[0], c[1], c[2], a,
+                x, y, r, g, b, a,
+                x + w, y, r, g, b, a,
+                x, y + lw, r, g, b, a,
 
-                x + w, y + lw, c[0], c[1], c[2], a,
-                x, y + lw, c[0], c[1], c[2], a,
-                x + w, y, c[0], c[1], c[2], a,
+                x + w, y + lw, r, g, b, a,
+                x, y + lw, r, g, b, a,
+                x + w, y, r, g, b, a,
 
-                x + w + lw, y, c[0], c[1], c[2], a,
-                x + w + lw, y + h, c[0], c[1], c[2], a,
-                x + w, y, c[0], c[1], c[2], a,
+                x + w + lw, y, r, g, b, a,
+                x + w + lw, y + h, r, g, b, a,
+                x + w, y, r, g, b, a,
 
-                x + w, y + h, c[0], c[1], c[2], a,
-                x + w, y, c[0], c[1], c[2], a,
-                x + w + lw, y + h, c[0], c[1], c[2], a,
+                x + w, y + h, r, g, b, a,
+                x + w, y, r, g, b, a,
+                x + w + lw, y + h, r, g, b, a,
 
-                x + w + lw, y + h + lw, c[0], c[1], c[2], a,
-                x + lw, y + h + lw, c[0], c[1], c[2], a,
-                x + w + lw, y + h, c[0], c[1], c[2], a,
+                x + w + lw, y + h + lw, r, g, b, a,
+                x + lw, y + h + lw, r, g, b, a,
+                x + w + lw, y + h, r, g, b, a,
 
-                x + lw, y + h, c[0], c[1], c[2], a,
-                x + w + lw, y + h, c[0], c[1], c[2], a,
-                x + lw, y + h + lw, c[0], c[1], c[2], a,
+                x + lw, y + h, r, g, b, a,
+                x + w + lw, y + h, r, g, b, a,
+                x + lw, y + h + lw, r, g, b, a,
 
-                x, y + h + lw, c[0], c[1], c[2], a,
-                x, y + lw, c[0], c[1], c[2], a,
-                x + lw, y + h + lw, c[0], c[1], c[2], a,
+                x, y + h + lw, r, g, b, a,
+                x, y + lw, r, g, b, a,
+                x + lw, y + h + lw, r, g, b, a,
 
-                x + lw, y + lw, c[0], c[1], c[2], a,
-                x + lw, y + h + lw, c[0], c[1], c[2], a,
-                x, y + lw, c[0], c[1], c[2], a,
+                x + lw, y + lw, r, g, b, a,
+                x + lw, y + h + lw, r, g, b, a,
+                x, y + lw, r, g, b, a
             ];
         }
 
-        public static float[] Outline(RectangleF rect, float lw, params float[] c)
-            => Outline(rect.X, rect.Y, rect.Width, rect.Height, lw, c);
-        public static float[] Outline(float x, float y, float w, float h, float lw, Color color, float alpha = 1)
-            => Outline(x, y, w, h, lw, color.R / 255f, color.G / 255f, color.B / 255f, alpha);
-        public static float[] Outline(RectangleF rect, float lw, Color color, float alpha = 1)
-            => Outline(rect.X, rect.Y, rect.Width, rect.Height, lw, color.R / 255f, color.G / 255f, color.B / 255f, alpha);
+        public static float[] Outline(RectangleF rect, float lw, float r, float g, float b, float a = 1)
+            => Outline(rect.X, rect.Y, rect.Width, rect.Height, lw, r, g, b, a);
+        public static float[] Outline(float x, float y, float w, float h, float lw, Color color, float a = 1)
+            => Outline(x, y, w, h, lw, color.R / 255f, color.G / 255f, color.B / 255f, color.A / 255f * a);
+        public static float[] Outline(RectangleF rect, float lw, Color color, float a = 1)
+            => Outline(rect.X, rect.Y, rect.Width, rect.Height, lw, color.R / 255f, color.G / 255f, color.B / 255f, color.A / 255f * a);
 
 
 
-        public static float[] Texture(float x, float y, float w, float h, float tx = 0, float ty = 0, float tw = 1, float th = 1, float alpha = 1)
+        public static float[] Texture(float x, float y, float w, float h, float tx = 0, float ty = 0, float tw = 1, float th = 1, float a = 1)
         {
             return
             [
-                x, y, tx, ty, alpha,
-                x + w, y, tx + tw, ty, alpha,
-                x, y + h, tx, ty + th, alpha,
+                x, y, tx, ty, a,
+                x + w, y, tx + tw, ty, a,
+                x, y + h, tx, ty + th, a,
 
-                x + w, y + h, tx + tw, ty + th, alpha,
-                x, y + h, tx, ty + th, alpha,
-                x + w, y, tx + tw, ty, alpha
+                x + w, y + h, tx + tw, ty + th, a,
+                x, y + h, tx, ty + th, a,
+                x + w, y, tx + tw, ty, a
             ];
         }
 
@@ -126,10 +122,8 @@ namespace New_SSQE.NewGUI
 
 
 
-        public static float[] Line(float x1, float y1, float x2, float y2, float lw, params float[] c)
+        public static float[] Line(float x1, float y1, float x2, float y2, float lw, float r, float g, float b, float a = 1)
         {
-            float a = c.Length == 4 ? c[3] : 1f;
-
             Vector2 start = (x1, y1);
             Vector2 end = (x2, y2);
             Vector2 dir = end - start;
@@ -142,33 +136,31 @@ namespace New_SSQE.NewGUI
             
             return
             [
-                startL.X, startL.Y, c[0], c[1], c[2], a,
-                endL.X, endL.Y, c[0], c[1], c[2], a,
-                endR.X, endR.Y, c[0], c[1], c[2], a,
+                startL.X, startL.Y, r, g, b, a,
+                endL.X, endL.Y, r, g, b, a,
+                endR.X, endR.Y, r, g, b, a,
 
-                endR.X, endR.Y, c[0], c[1], c[2], a,
-                startR.X, startR.Y, c[0], c[1], c[2], a,
-                startL.X, startL.Y, c[0], c[1], c[2], a,
+                endR.X, endR.Y, r, g, b, a,
+                startR.X, startR.Y, r, g, b, a,
+                startL.X, startL.Y, r, g, b, a
             ];
         }
 
-        public static float[] Line(float x1, float y1, float x2, float y2, float lw, Color color, float alpha = 1)
-            => Line(x1, y1, x2, y2, lw, color.R / 255f, color.G / 255f, color.B / 255f, alpha);
+        public static float[] Line(float x1, float y1, float x2, float y2, float lw, Color color, float a = 1)
+            => Line(x1, y1, x2, y2, lw, color.R / 255f, color.G / 255f, color.B / 255f, color.A / 255f * a);
 
 
 
-        public static float[] Polygon(float x, float y, float radius, int sides, float angle, params float[] c)
+        public static float[] Polygon(float x, float y, float radius, int sides, float angle, float r, float g, float b, float a = 1)
         {
-            float alpha = c.Length == 4 ? c[3] : 1f;
-
             Vector2[] vertices = new Vector2[sides];
             double rad = MathHelper.DegreesToRadians(angle);
 
             for (int i = 0; i < sides; i++)
             {
-                double a = rad + i / (float)sides * Math.PI * 2;
-                double vx = Math.Cos(a) * radius;
-                double vy = -Math.Sin(a) * radius;
+                double ar = rad + i / (float)sides * Math.PI * 2;
+                double vx = Math.Cos(ar) * radius;
+                double vy = -Math.Sin(ar) * radius;
 
                 vertices[i] = ((float)vx + x, (float)vy + y);
             }
@@ -182,49 +174,47 @@ namespace New_SSQE.NewGUI
 
                 final[index++] = vertices[i].X;
                 final[index++] = vertices[i].Y;
-                final[index++] = c[0];
-                final[index++] = c[1];
-                final[index++] = c[2];
-                final[index++] = alpha;
+                final[index++] = r;
+                final[index++] = g;
+                final[index++] = b;
+                final[index++] = a;
 
                 final[index++] = x;
                 final[index++] = y;
-                final[index++] = c[0];
-                final[index++] = c[1];
-                final[index++] = c[2];
-                final[index++] = alpha;
+                final[index++] = r;
+                final[index++] = g;
+                final[index++] = b;
+                final[index++] = a;
 
                 final[index++] = vertices[next].X;
                 final[index++] = vertices[next].Y;
-                final[index++] = c[0];
-                final[index++] = c[1];
-                final[index++] = c[2];
-                final[index++] = alpha;
+                final[index++] = r;
+                final[index++] = g;
+                final[index++] = b;
+                final[index++] = a;
             }
 
             return final;
         }
 
         public static float[] Polygon(float x, float y, float radius, int sides, float angle, Color color, float alpha = 1)
-            => Polygon(x, y, radius, sides, angle, color.R / 255f, color.G / 255f, color.B / 255f, alpha);
+            => Polygon(x, y, radius, sides, angle, color.R / 255f, color.G / 255f, color.B / 255f, color.A / 255f * alpha);
 
 
 
-        public static float[] PolygonOutline(float x, float y, float radius, float width, int sides, float angle, params float[] c)
+        public static float[] PolygonOutline(float x, float y, float radius, float width, int sides, float angle, float r, float g, float b, float a = 1)
         {
-            float alpha = c.Length == 4 ? c[3] : 1f;
-
             Vector4[] vertices = new Vector4[sides];
             double rad = MathHelper.DegreesToRadians(angle);
 
             for (int i = 0; i < sides; i++)
             {
-                double a = rad + i / (float)sides * Math.PI * 2;
-                float vx1 = (float)Math.Cos(a) * (radius + width / 2);
-                float vy1 = (float)-Math.Sin(a) * (radius + width / 2);
+                double ar = rad + i / (float)sides * Math.PI * 2;
+                float vx1 = (float)Math.Cos(ar) * (radius + width / 2);
+                float vy1 = (float)-Math.Sin(ar) * (radius + width / 2);
 
-                float vx2 = (float)Math.Cos(a) * (radius - width / 2);
-                float vy2 = (float)-Math.Sin(a) * (radius - width / 2);
+                float vx2 = (float)Math.Cos(ar) * (radius - width / 2);
+                float vy2 = (float)-Math.Sin(ar) * (radius - width / 2);
 
                 vertices[i] = (vx1 + x, vy1 + y, vx2 + x, vy2 + y);
             }
@@ -238,51 +228,51 @@ namespace New_SSQE.NewGUI
 
                 final[index++] = vertices[i].X;
                 final[index++] = vertices[i].Y;
-                final[index++] = c[0];
-                final[index++] = c[1];
-                final[index++] = c[2];
-                final[index++] = alpha;
+                final[index++] = r;
+                final[index++] = g;
+                final[index++] = b;
+                final[index++] = a;
 
                 final[index++] = vertices[i].Z;
                 final[index++] = vertices[i].W;
-                final[index++] = c[0];
-                final[index++] = c[1];
-                final[index++] = c[2];
-                final[index++] = alpha;
+                final[index++] = r;
+                final[index++] = g;
+                final[index++] = b;
+                final[index++] = a;
 
                 final[index++] = vertices[next].X;
                 final[index++] = vertices[next].Y;
-                final[index++] = c[0];
-                final[index++] = c[1];
-                final[index++] = c[2];
-                final[index++] = alpha;
+                final[index++] = r;
+                final[index++] = g;
+                final[index++] = b;
+                final[index++] = a;
 
                 final[index++] = vertices[next].X;
                 final[index++] = vertices[next].Y;
-                final[index++] = c[0];
-                final[index++] = c[1];
-                final[index++] = c[2];
-                final[index++] = alpha;
+                final[index++] = r;
+                final[index++] = g;
+                final[index++] = b;
+                final[index++] = a;
 
                 final[index++] = vertices[next].Z;
                 final[index++] = vertices[next].W;
-                final[index++] = c[0];
-                final[index++] = c[1];
-                final[index++] = c[2];
-                final[index++] = alpha;
+                final[index++] = r;
+                final[index++] = g;
+                final[index++] = b;
+                final[index++] = a;
 
                 final[index++] = vertices[i].Z;
                 final[index++] = vertices[i].W;
-                final[index++] = c[0];
-                final[index++] = c[1];
-                final[index++] = c[2];
-                final[index++] = alpha;
+                final[index++] = r;
+                final[index++] = g;
+                final[index++] = b;
+                final[index++] = a;
             }
 
             return final;
         }
 
-        public static float[] PolygonOutline(float x, float y, float radius, float width, int sides, float angle, Color color, float alpha = 1)
-            => PolygonOutline(x, y, radius, width, sides, angle, color.R / 255f, color.G / 255f, color.B / 255f, alpha);
+        public static float[] PolygonOutline(float x, float y, float radius, float width, int sides, float angle, Color color, float a = 1)
+            => PolygonOutline(x, y, radius, width, sides, angle, color.R / 255f, color.G / 255f, color.B / 255f, color.A / 255f * a);
     }
 }
