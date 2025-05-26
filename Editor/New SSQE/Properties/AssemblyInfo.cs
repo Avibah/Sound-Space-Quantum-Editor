60eea42ce9a023ee2051209bb589e9f0dd4da657
Avibah <redstoneminecraft45@gmail.com>
2025-05-25 21:03:51 -0500

    update copyright year

--- Editor/New SSQE/Properties/AssemblyInfo.cs
+++ Editor/New SSQE/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Sound Space Studios")]
 [assembly: AssemblyProduct("Sound Space Editor")]
-[assembly: AssemblyCopyright("Copyright © 2024")]
+[assembly: AssemblyCopyright("Copyright © 2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
